9c6f6bb993bd775e9ec5a47337237b3923d3a526
Mykhailo Aliinyk <m.aliynik@gmail.com>
2021-12-21 20:41:37 +0200

    Removed redundant deps

--- netlify/functions/make-order.ts
+++ netlify/functions/make-order.ts
@@ -1,10 +1,12 @@
 import { Handler } from '@netlify/functions';
 import { Telegraf } from 'telegraf';
 
+const CHAT_ID = 5033789131;
+
 const app = new Telegraf(process.env.BOT_TOKEN);
 
 const handler: Handler = async (event, context) => {
-  await app.telegram.sendMessage(5033789131, 'order');
+  await app.telegram.sendMessage(CHAT_ID, 'order');
 
   return {
     statusCode: 200,
