9bd42a0d58fdf1ea55c7f8d8d93d4e3d940110d6
Mykhailo Aliinyk <m.aliynik@gmail.com>
2024-01-14 15:52:09 +0000

    fixes to generate google merchant products

--- generators/products-generator.ts
+++ generators/products-generator.ts
@@ -1,26 +1,48 @@
 import { writeFileSync, appendFileSync } from 'fs';
 import { getProductLink, readDb } from './helpers';
 import { generateItemId } from '../src/utils';
+import { SITE_DOMAIN } from './constants';
 
 const PRODUCTS_FILENAME = 'products.txt';
 
-const COLUMNS = ['id', 'title', 'link'];
+const COLUMNS = ['id', 'title', 'description', 'price', 'availability', 'image_link', 'link'];
 
-type Products = string[][];
+type Products = (string | number)[][];
 
 const run = () => {
   const productsData: Products = [];
 
   readDb(dbPart => {
     dbPart.items.forEach(item => {
-      productsData.push([generateItemId(item.title), item.title, getProductLink(item)]);
+      if (typeof item.price !== 'number') {
+        return;
+      }
+
+      if (!item.images.length) {
+        console.warn(`Images are missing for item "${item.title}"`);
+        return;
+      }
+
+      if (item.seo) {
+        productsData.push([
+          generateItemId(item.title),
+          `"${item.title}"`,
+          `"${item.seo.description}"`,
+          item.price,
+          item.availability ? 'in_stock' : 'out_of_stock',
+          item.images[0].src.startsWith('http')
+            ? item.images[0].src
+            : `${SITE_DOMAIN}/item` + item.images[0].src,
+          getProductLink(item),
+        ]);
+      }
     });
   });
 
-  writeFileSync(PRODUCTS_FILENAME, `${COLUMNS.join('  ')}\n`);
+  writeFileSync(PRODUCTS_FILENAME, `${COLUMNS.join('\t')}\n`);
 
   productsData.forEach(product => {
-    appendFileSync(PRODUCTS_FILENAME, `${product.map(p => `"${p}"`).join('  ')}\n`);
+    appendFileSync(PRODUCTS_FILENAME, `${product.join('\t')}\n`);
   });
 };
 
